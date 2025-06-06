e34dabbca82bebcd35face63b600fdb25bc34885
Danilo Galisteu <danilogalisteu@gmail.com>
2025-06-06 15:07:49 -0300

    parsing args

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("Error: no website provided\nUsage: ./crawler <url>")
+		os.Exit(1)
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("Error: too many arguments provided\nUsage: ./crawler <url>")
+		os.Exit(1)
+	}
+
+	baseUrl := os.Args[1]
+	fmt.Printf("starting crawl of: %s\n", baseUrl)
 }
