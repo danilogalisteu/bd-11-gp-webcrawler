894a4f0aeb5bb0dbcba8d23d6dbf2060b61e1f21
Danilo Galisteu <danilogalisteu@gmail.com>
2025-06-06 15:41:35 -0300

    crawl page and update

---
+++ crawl_page.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+)
+
+func checkSameDomain(baseURL, currentURL string) (bool, error) {
+	partsBase, err := url.Parse(baseURL)
+	if err != nil {
+		return false, err
+	}
+	partsCurrent, err := url.Parse(currentURL)
+	if err != nil {
+		return false, err
+	}
+	return partsBase.Hostname() == partsCurrent.Hostname(), nil
+}
+
+func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
+	sameDomain, err := checkSameDomain(rawBaseURL, rawCurrentURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !sameDomain {
+		return
+	}
+
+	normUrl, err := normalizeURL(rawCurrentURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	count, exists := pages[normUrl]
+	if exists {
+		pages[normUrl] = count + 1
+		return
+	}
+	pages[normUrl] = 1
+
+	fmt.Printf("crawling %s...\n", rawCurrentURL)
+	html, err := getHTML(rawCurrentURL)
+	if err != nil {
+		fmt.Printf("%s: non-HTML link found, skipping...\n", rawCurrentURL)
+	}
+
+	urls, err := getURLsFromHTML(html, rawBaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, url := range urls {
+		fmt.Printf("checking %s...\n", url)
+		crawlPage(rawBaseURL, url, pages)
+	}
+}
